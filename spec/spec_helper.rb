7b28b6a670d94f9967a2e201b97b9761eb6436af
Luis <nanocity@MarkIV.(none)>
2011-03-31 10:10:43 +0200

    Spec_helper arreglado

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 $LOAD_PATH << File.expand_path('../../examples_helpers', __FILE__) 
 
 require 'command'
 require 'runnable'
-=======
-$LOAD_PATH << File.expand_path('../lib/', __FILE__)
  
-#require 'runnable'
-
->>>>>>> 2cb33747c9a09156f684ff763689c0a5e33707e3
