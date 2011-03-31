c52f66da23bd022f8ff9ef94fe505276eb02f516
Luis Ciudad <lciudad@nosolosoftware.biz>
2011-03-31 10:06:24 +0200

    Env.rb arreglado

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,7 @@
 $LOAD_PATH << File.expand_path('../../../lib/', __FILE__)
-<<<<<<< HEAD
 $LOAD_PATH << File.expand_path('../../../examples_helpers/', __FILE__)
  
-require 'command'
-=======
- 
+require 'command' 
 #require 'runnable'
->>>>>>> 2cb33747c9a09156f684ff763689c0a5e33707e3
+
 
