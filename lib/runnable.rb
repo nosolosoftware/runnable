5a584486c096ffbb4f02fef39a3975220abdf7e1
Rafael Garcia <rgarcia@nosolosoftware.biz>
2011-04-04 13:44:01 +0200

    Añadidos comentarios basicos de yard para autodocumentacion en lib/runnable.rb

--- lib/runnable.rb
+++ lib/runnable.rb
@@ -1,24 +1,42 @@
+# Convert a executable command in a Ruby-like class
+# you are able to start, send signals, stop the command
+#
+# @example usage:
+#   class LS < Runnable
+#   end
+#
+#   ls = LS.new
+#   ls.start
+#
 class Runnable
   attr_reader :pid
   
+  # 
   def initialize( )
     @command = self.class.to_s.to_lower
     
-    # checks that command is in the PATH
+    # @todo: checks that command is in the PATH
     # ...
     
     @pid = nil
   end
   
+  # Start the command
   def run
     proc = IO.popen( @command )
     @pid = proc.pid
   end
   
+  # Stop the command 
+  # @return nil
+  # @todo: @raise exeption
   def stop
     send_signal( :stop )
   end
   
+  # Kill the comand
+  # @return nil
+  # @todo: @raise exeption
   def kill
     send_signal( :kill )
   end
@@ -26,6 +44,9 @@ class Runnable
   
   protected
   
+  # Send the desired signal to the command
+  # @param [Symbol] signal must be a symbol
+  # @todo: @raise exeption
   def send_signal( signal )
     if signal == :stop
       Process.kill( :SIGINT, @pid )
