3f78994352f783c3166fd1ec8757ad5c7e08ee48
Luis Ciudad <lciudad@nosolosoftware.biz>
2011-05-04 13:08:48 +0200

    #54 Documentación de las clases de parseo command_parser y gnu

--- lib/runnable/gnu.rb
+++ lib/runnable/gnu.rb
@@ -6,6 +6,7 @@ class Gnu < Command_parser
   # This method convert a hash {parameter => value} in a string ready to
   # be passed to a command that uses GNU style to parse command line
   # parameters
+  # @return [String] Gnu-style parsed params in a raw character array
   def parse
     result = ""
 
@@ -14,6 +15,10 @@ class Gnu < Command_parser
       # lead and two or more characters words are preceed by two 
       # leads
       result << ( param.length == 1 ? "-#{param} " : "--#{param} " )
+
+      # In case the param have parameter we use the correct assignation
+      #   -Param followed by value (without whitespace) to one character params
+      #   -Param followed by '=' and value to more than one character params
       if( value != nil )
         result << ( param.length == 1 ? "#{value}" : "=#{value}" )
       end
