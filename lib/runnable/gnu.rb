4a9ed76bba5b5ca124d1259041657843b0c67824
Luis Ciudad <lciudad@nosolosoftware.biz>
2011-05-04 13:54:43 +0200

    Creado el archivo de licencia GPL. En el código fuente que se va a distribuir se incluyen las cabeceras de la licencia necesarias. No se incluyen estas cabeceras en ficheros fuente que no se publicarán, por ejemplo los del directorio spec

--- lib/runnable/gnu.rb
+++ lib/runnable/gnu.rb
@@ -1,3 +1,20 @@
+# Copyright 2011 NoSoloSoftware
+
+# This file is part of Runnable.
+# 
+# Runnable is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Runnable is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Runnable.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'command_parser'
 
 # Parse the parameter hash using the GNU standard
